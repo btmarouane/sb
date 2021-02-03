9ddab6287fb536889b88b5ab12bab88e2ff162cf
salty <salty@salty.dk>
2021-02-03 17:08:03 +0100

    Enable settings skip.

--- cb.sh
+++ cb.sh
@@ -114,8 +114,8 @@ install () {
     # Build CB/CM tag arrays
     local tags_cb
     local tags_cm
-    local skip_settings_in_cb=false
-    local skip_settings_in_cm=false
+    local skip_settings_in_cb=true
+    local skip_settings_in_cm=true
 
     for i in "${!tags[@]}"
     do
