fec2be2d92516a5c6bc72623c3480de96a6bd89f
salty <salty@salty.dk>
2021-06-16 17:02:37 +0200

    Bump pip timeout value to 2 minutes.

--- sb_dep.sh
+++ sb_dep.sh
@@ -29,6 +29,7 @@ VERBOSE=true
 
 readonly SYSCTL_PATH="/etc/sysctl.conf"
 readonly PYTHON_CMD_SUFFIX="-m pip install \
+                              --timeout=120 \
                               --no-cache-dir \
                               --disable-pip-version-check \
                               --upgrade"
